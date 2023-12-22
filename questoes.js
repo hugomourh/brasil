b97d3843bd6961076d48993303e50bdc63f64616
hugomourh <mourah689@gmail.com>
2023-12-21 22:57:57 -0300

    first commit

---
+++ questoes.js
@@ -0,0 +1,43 @@
+export default [
+    {
+      question: "Em que ano descobriu o Brasil?",
+      answers: [
+        { option: "1500", correct: true },
+        { option: "1900", correct: false },
+        { option: "1800", correct: false },
+      ],
+    },
+    {
+      question: "Figura principal da Inconfidência Mineira?",
+      answers: [
+        { option: "Getulio Vargas", correct: false },
+        { option: "Tiradentes", correct: true },
+        { option: "Cristovão Colombo", correct: false },
+      ],
+    },
+    {
+      question: "O Brasil matou cerca de 80% da população Paraguaia?",
+      answers: [
+        { option: "Não", correct: false },
+        { option: "Sim", correct: true },
+      ],
+    },
+    {
+      question: "Quem foi o Imperador do Brasil no segundo reinado?",
+      answers: [
+        { option: "General Castelo Branco", correct: false },
+        { option: "Temer", correct: false },
+        { option: "Dom Pedro II", correct: true },
+      ],
+    },
+
+    {
+        question: "Quantos anos durou a Ditadura Militar Brasileira?",
+        answers: [
+          { option: "21 anos", correct: true },
+          { option: "60 anos", correct: false },
+          { option: "2 anos", correct: false },
+        ],
+      },
+  ];
+
